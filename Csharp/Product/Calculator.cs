544bc0412c03f3b21e9908448fa2022432fd880a
Mike Durst <mdurst@energypluscompany.com>
2013-02-12 14:52:32 -0500

    Update Calculator

--- Csharp/Product/Calculator.cs
+++ Csharp/Product/Calculator.cs
@@ -5,6 +5,8 @@ namespace Product
     {
         public static int Add(string numbers)
         {
+            if (String.IsNullOrWhiteSpace(numbers)) return 0;
+
             return -1;
         }
     }
