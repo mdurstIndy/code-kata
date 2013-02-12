ccc3efff29e4dfdf0bca816752fb53bd349263eb
Mike Durst <mdurst@energypluscompany.com>
2013-02-12 14:12:40 -0500

    Some changes for the kotta

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -3,4 +3,12 @@ describe("Add", function() {
     var result = Add("");
     expect(result).toEqual(0);
     });
+  it("should return the number for a number input", function() {
+    var result = Add('1');
+    expect(result).toEqual(1);
+    });
+  it("should return the number for a sequence of numbers input", function() {
+    var result = Add('1,2');
+    expect(result).toEqual(3);
+    });
 });
--- javascript/src/Calculator.js
+++ javascript/src/Calculator.js
@@ -1,3 +1,12 @@
-function Add(numbers) {
-	return -1;
+function Add(stringOfNumbers) {
+	if ( stringOfNumbers === "") return 0;
+
+	var stringArray = stringOfNumbers.split(",");
+
+	var sum = 0;
+	for (var i = stringArray.length - 1; i >= 0; i--) {
+		sum = sum + parseInt(stringArray[i]);
+	};
+
+	return sum;
 }
